04be316f5e6d611e02d472eeb217a2467dc14f37
ben slimen eya <slimenaya79@gmail.com>
2024-02-14 14:48:13 +0100

    product_card and footer

--- ReCraftMartket/src/app/app-routing.module.ts
+++ ReCraftMartket/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
+import { FooterComponent } from './footer/footer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+
 import path from 'path';
+
 const routes: Routes = [
-  {path:"navbar", component:NavbarComponent }
+  {path:"navbar", component:NavbarComponent },
+  {path:"product_Card",component:ProductCardComponent},
+  {path:"footer",component:FooterComponent}
 ];
 
 @NgModule({
--- ReCraftMartket/src/app/app.module.ts
+++ ReCraftMartket/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [
     provideClientHydration()
