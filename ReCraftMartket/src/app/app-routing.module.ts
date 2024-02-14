92e14e6da773287b0dbf5e22e6edded5ceddf7f9
matoussi roua <85041175+matoussi-roua@users.noreply.github.com>
2024-02-14 02:42:40 +0100

    sidebar and profile

--- ReCraftMartket/src/app/app-routing.module.ts
+++ ReCraftMartket/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import path from 'path';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
+
 const routes: Routes = [
-  {path:"navbar", component:NavbarComponent }
+  {path:"navbar", component:NavbarComponent },
+  {path:"sidebar",component:SidebarComponent},
+  {path:"profile",component:ProfileComponent}
 ];
 
 @NgModule({
--- ReCraftMartket/src/app/app.module.ts
+++ ReCraftMartket/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
